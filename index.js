73edc70df62afb3ac2e6b75297774a0a967b3bad
Gautier Darchen <gdarchen@takima.fr>
2018-10-25 11:36:49 +0200

    feat: update index.js to load dynamically the node modules

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
+const fs = require("fs");
+const path = require("path");
+
 module.exports = function nodeLoader() {
-  return (
-    `try {global.process.dlopen(module, ${JSON.stringify(
-      this.resourcePath
-    )}); } catch(e) {` +
-    `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
-      this.resourcePath
-    )} + ': ' + e);}`
+  const filename = path.basename(this.resourcePath);
+  const src = fs.readFileSync(this.resourcePath, {encoding: "binary"});
+  return `
+const nodeFilename = ${JSON.stringify(filename)};
+const src = ${JSON.stringify(src)};
+require("fs").writeFileSync(nodeFilename, src, "binary");
+try {
+  global.process.dlopen(
+    module,
+    nodeFilename
   );
+} catch(e) {
+  throw new Error('node-loader: Cannot open ' + nodeFilename + ': ' + e);
+}
+`;
 };
